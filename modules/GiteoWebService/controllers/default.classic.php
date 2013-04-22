56a39558502b110492ff040bca9583e5472758e4
francoisdelespinay <francois.delespinay@gmail.com>
2013-04-22 19:50:17 +0200

    Ajout de toutes les daos et fin du code get

--- modules/GiteoWebService/controllers/default.classic.php
+++ modules/GiteoWebService/controllers/default.classic.php
@@ -10,10 +10,10 @@
  */
 class defaultCtrl extends jController {
 
-    /**
-     *
-     */
+
     function index() {
+        
+
         $response = $this->getResponse('json');
         $monURL = $_SERVER['REQUEST_URI'];
         $urlquery = parse_url($monURL, PHP_URL_QUERY);
@@ -21,56 +21,52 @@ class defaultCtrl extends jController {
         var_dump($request);
         switch ($request[0]) {
             case "GET":
-              
-                //$response = gmethod($request);
-                $Factory = jDao::get($request[1]);
 
 
 
 
-        $conditions = jDao::createConditions();
-        foreach (explode("&", $request[2]) as $chunk) {
-            $param = explode("=", $chunk);
-            //var_dump($param);
-            if ($param[1] == "?") {
-                $listOfAll = $Factory->findAll();
-            } else {
-                $conditions->addCondition($param[0], '=', $param[1]);
-                $listOfAll = $Factory->findBy($conditions);
-            }
-        }
-//
-        $chemin='../../GiteoWebService/modules/GiteoWebService/daos/'.$request[1].'.dao.xml';
-
-        //$xmldao = simplexml_load_file($chemin);
-
-          $xmldao = new DomDocument;
-          $xmldao->load($chemin);
-
-          $Listeprop = $xmldao->getElementsByTagName("property");
-         // $champs->getAttribute("name");
-            var_dump($Listeprop);
-
-            
-            //echo($chaine);
-
-        $response->data = array();
 
 
-      
+                foreach (explode("&", $request[2]) as $chunk) {
+                    $param = explode("=", $chunk);
+                    //var_dump($param);
+                    $Factory = jDao::get($request[1]);
+                    if ($param[1] == "?") {
+                      
+                        $listOfAll = $Factory->findAll();
+                    } else {
+                        
+                        $conditions = jDao::createConditions();
+                        $conditions->addCondition($param[0], '=', $param[1]);
+                        $listOfAll = $Factory->findBy($conditions);
+                    }
+                }
 
-        foreach ($listOfAll as $promo) {
-//'id_promotion'=> $promo->id_promotion, 'prm_code'=> $promo->prm_code, 'prm_dateBegin'=> $promo->prm_dateBegin, 'prm_dateEnd'=> $promo->prm_dateEnd, 'prm_commissionRate'=> $promo->prm_commissionRate, 'prm_nbUsesLimit'=> $promo->prm_nbUsesLimit, 'prm_comment'=> $promo->prm_comment,
-                  $chaine="";
-            foreach ($Listeprop as $Listename){
-               // $chaine=$chaine."'".$Listename->getAttribute("name")."'".'=>'+$promo+'->'.$Listename->getAttribute("name").', ';
-                $response->data[] = array("'".$Listename->getAttribute("name")."'".'=>'+$promo+'->'.$Listename->getAttribute("name").', ');                
-            }
+                var_dump($request[1]);
 
-        }
 
-  
-        
+                $xmldao = new DomDocument;
+                $chemin = '../../GiteoWebService/modules/GiteoWebService/daos/'.$request[1].'.dao.xml';
+                $xmldao->load($chemin);
+                
+                $Listeprop = $xmldao->getElementsByTagName("property");
+                var_dump($Listeprop);
+
+                $response->data = array();
+
+                foreach ($listOfAll as $val) {
+                    $chaine = array();
+                    foreach ($Listeprop as $Listename) {
+
+                        $toEval = '$var = $val->' . $Listename->getAttribute("name") . ';';
+                        eval($toEval);
+                        $chaine[$Listename->getAttribute("name")] = $var;
+                    }
+                    $response->data[] = $chaine;
+                 
+                }
+
+
 
                 break;
             case "POST":
@@ -86,17 +82,11 @@ class defaultCtrl extends jController {
 
                 break;
         }
-        
+
 
 
         return $response;
     }
-    
-   // function gmethod($request){
-    
-    
-        
-   // }
 
 }
 
